1f105e0be195944142226c4bb4d9020e6861a6f1
Yasmin002 <alvesyasmin094@gmail.com>
2021-06-24 06:36:33 +0900

    Tentativa 3

--- www/lib/script.js
+++ www/lib/script.js
@@ -1,14 +1,16 @@
 // This is a JavaScript file
 window.onload = function (){
-  const buscar = document.querySelector("#buscar");
-  const cep = document.querySelector("#cep");
+const buscar = document.querySelector("#buscar");
+
+const cep = document.querySelector("#cep");
+
     const opcoes = {
     method: 'GET',
     mode: 'cors',
     cache: 'default'
   }
   buscar.addEventListener("click", function(){
-    fetch('https://viacep.com.br/ws/${ cep.value }/json/',opcoes)
+    fetch('https://viacep.com.br/ws/${cep.value}/json/',opcoes)
     .then(response => {response.json()
     .then(data => {
      document.querySelector("#estado").value = data['uf'];
@@ -18,5 +20,5 @@ window.onload = function (){
      
        })
     })
-  })
+  });
 }
